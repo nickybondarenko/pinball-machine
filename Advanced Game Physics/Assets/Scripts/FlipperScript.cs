c8baf0b350bed4aef06aaaecd44605c66f1008ff
vbondarenko1 <vbondarenko@babbel.com>
2019-05-05 17:29:44 +0200

    flippers setup
    The flippers have been done. The bounds leading to flipper have been done.

---
+++ Advanced Game Physics/Assets/Scripts/FlipperScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipperScript : MonoBehaviour
+{
+    //These values can be changed in the inspector
+    public float restPosition = 0f;
+    public float pressedPosition = 45f;
+    public float hitStrength = 10000f;
+    public float flipperDamper = 150f;
+    public string inputName;
+
+    HingeJoint hinge;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hinge = GetComponent<HingeJoint>();
+        hinge.useSpring = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        JointSpring spring = new JointSpring();
+        spring.spring = hitStrength;
+        spring.damper = flipperDamper;
+
+        //Check if the button is pressed
+        if (Input.GetAxis(inputName) == 1)
+        {
+            spring.targetPosition = pressedPosition;
+        }
+        else
+        {
+            spring.targetPosition = restPosition;
+        }
+
+        hinge.spring = spring;
+        hinge.useLimits = true;
+
+    }
+}
