31e8f4eeab7b75476efbb9651ac2605eb7655677
vbondarenko1 <31034418+vbondarenko1@users.noreply.github.com>
2019-06-20 14:40:51 +0200

    added new vfx

---
+++ New Unity Project/Assets/Scripts/RespawnScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RespawnScript : MonoBehaviour
+{
+    [SerializeField] private Transform ball;
+    [SerializeField] private Transform respawnPoint;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        ball.transform.position = respawnPoint.transform.position;
+    }
+}
---
+++ New Unity Project/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    public Text ScoreField;
+    public int Score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Score = 0;
+        ScoreFieldUpdate();
+    }
+
+    public void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle") {
+            Score = Score + 100;
+            ScoreFieldUpdate();
+        }
+    }
+
+    private void ScoreFieldUpdate(){
+        ScoreField.text = "Punkte: " + Score.ToString();
+    }
+}
